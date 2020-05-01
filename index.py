c02c7358b37a8d7651461f78a4a802c2f6a0c8f1
topherjamesknoll <topherjamesknoll@gmail.com>
2020-04-30 17:01:05 -0700

    Horray!

--- index.py
+++ index.py
@@ -1,29 +1,33 @@
 # Project 1 - Number Guessing Game
 
-SECRET_NUMBER = 76
-
 def start_game():
-
     tries = 0
     print("Welcome to the Number Guessing Game!!!")
     while True:
         try:
-            guess = input("Guess a number between 1 and 100 > ")
-            if guess > 100:
-                raise NameError
-            if guess < 1:
-                raise NameError
-        except NameError:
-            print("Sorry that is not a valid number.")
+            secret_number = int(input("Enter a secret number > "))
+        except ValueError:
+            print("Sorry; that is not a valid number.")
         else:
-            if guess > SECRET_NUMBER:
-                guess = input("It's lower. Try again > ")
-            elif guess < SECRET_NUMBER:
-                guess = input("It's higher. Try again > ")
-            else:
-                print("Got it! You won in {} tries!").format(tries)
-                break
-            tries += 1
+            while True:
+                try:
+                    guess = int(input("Guess a number between 1 and 100 > "))
+                    if guess > 100:
+                        raise ValueError
+                    elif guess < 1:
+                        raise ValueError
+                except ValueError:
+                    print("Sorry that is not a valid number.")
+                else:
+                    if guess > secret_number:
+                        print("It's lower.")
+                    elif guess < secret_number:
+                        print("It's higher.")
+                    else:
+                        print("Got it! You won in", tries, "tries!")
+                        break
+                    tries += 1
+            break
     print("Thanks for playing!!!")
 
 start_game()
